ba6fbac79860db095856bd3423be6a78010483ae
Gulshansiharas <144225879+Gulshansiharas@users.noreply.github.com>
2023-09-30 21:31:10 +0530

    Add files via upload
    This is the problem done on interchanging the first and last elements in the list and  finding the squares of even and odd numbers in the list

---
+++ practice.py
@@ -0,0 +1,57 @@
+# interchange the 1st and last element
+#Method:1(built in function)
+list=[2,3,4,78,'hi','ji','kiash']
+# list.remove(2)
+# list.remove('kiash')
+# print(list)
+# list.insert(0,'kiash')
+# list.insert(6,2)
+# print(list)
+#Method-2(using pop function)
+# list.pop()
+# print(list)
+# list.pop(0)
+# print(list)
+# list.insert(0,'kiash')
+# list.insert(6,2)
+# print(list)
+#Method=3(slicing)
+# f=list[1:6]
+# print(f)
+# f.append(2)
+# print(f)
+# f.insert(0,'kiash')
+# print("The interchange list is :",f)
+
+# # To find the square of odd and even numbers
+# odd_square=[]
+# for x in range(1,11):
+#     if x%2==1:
+#         odd_square.append(x**2)
+# print(odd_square)
+# even_square=[]
+# for y in range(1,101):
+#     if y%2==0:
+#         even_square.append(y**2)
+# print(even_square)
+
+
+
+
+
+
+
+
+#method 2
+# list=[]
+# for i in range(1,100,10):
+#     list.append(i)
+#
+#
+#
+# print(list)
+
+
+
+
+
