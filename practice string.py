f82d7d695057cab8c1bace1ce2c56b25c597f5dc
Gulshansiharas <144225879+Gulshansiharas@users.noreply.github.com>
2023-09-30 23:22:41 +0530

    Add files via upload

---
+++ practice string.py
@@ -0,0 +1,45 @@
+#swap two elements in a list
+# l=[2,23,45,67,12,45,'fds']
+# l.remove(45)
+# l.remove(67)
+# print(l)
+# l.insert(2,67)
+# l.insert(3,45)
+# print(l)
+#find length of a list in python
+#method-1
+# j=[2,33,45,'fgg',5,6, 7]
+# print("The length of the list is",len(j))
+#method-2
+# s=0
+# for i in enumerate(j):
+#     s+=1
+# print(s)
+#swap elements in string list
+#method-1
+# l1=['python','is','awesome']
+# d=l1[0]
+# print(d)
+# g=l1[1]
+# print(g)
+# h=g
+# f=d
+# j=l1[2]
+# l=[]
+# l.append(h)
+# l.append(f)
+# l.append(j)
+# print(l)
+#Maximum of 2 numbers
+# l=23
+# b=34
+# print(max(l,b))
+#Minimum of 2 numbers
+# a=12
+# k=9
+# print(min(a,k))
+
+
+
+
+
